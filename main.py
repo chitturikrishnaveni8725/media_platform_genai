3e760dbee8db15340d58bf2555db36e94696b333
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-05-22 12:04:48 +0530

    view files added

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ def dashboard():
         if choosedFile:
             st.write(choosedFile.name)
             st.write(choosedFile.type)
-        if "image" in choosedFile.type:
-            st.image(choosedFile)
-        elif "video" in choosedFile.type:
-            st.video(choosedFile)
-        elif "audio" in choosedFile.type:
-            st.audio(choosedFile)    
+            if "image" in choosedFile.type:
+                st.image(choosedFile)
+            elif "video" in choosedFile.type:
+                st.video(choosedFile)
+            elif "audio" in choosedFile.type:
+                st.audio(choosedFile)    
                 
             
             
