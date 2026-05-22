a5d60cf3563acc2af60ae9d6026054c5c87cb29c
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-05-22 10:00:19 +0530

    created main.py files and wtote code for signup and login

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+login,signup=st.tabs(
+                ["Login","SignUp"]  
+                )
+
+        
+with login:
+    st.header("Login")
+    with st.form("Login_Form"):
+        email=st.text_input("Email")
+        password=st.text_input("Password",type="password")
+        btn=st.form_submit_button("Login")
+     
+        
+with signup:
+    st.header("SignUp")
+    with st.form("SignUp_form"):
+        name=st.text_input("Name")
+        Email=st.text_input("Email")
+        password=st.text_input("Password",type="password")
+        btn=st.form_submit_button("SignUp")
+        
