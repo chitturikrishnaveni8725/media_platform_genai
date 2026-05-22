6dafa582bcf1ec0274b4ebe8ec4e672f0dacc7f8
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-05-22 11:29:56 +0530

    databoard created

--- main.py
+++ main.py
@@ -6,22 +6,35 @@ st.title("Media Platform")
 login,signup=st.tabs(
                 ["Login","SignUp"]  
                 )
+if "user" not in st.session_state:
+    st.session_state.user=None
 
 
-cursor_obj.execute("show tables")
-dbs=cursor_obj.fetchall()
-for db in dbs:
-    st.write(db)
-        
-with login:
+def dashboard():
+    st.sidebar.success("welcome user")
+    st.header("dashboard")
+    
+    
+    
+
+def login_function():
     st.header("Login")
     with st.form("Login_Form"):
         email=st.text_input("Email")
         password=st.text_input("Password",type="password")
         btn=st.form_submit_button("Login")
+        if btn:
+            query="select * from users3 where email=%s and password=%s"
+            values=(email,password)
+            cursor_obj.execute(query,values)
+            loggedin_user=cursor_obj.fetchone()
+            st.session_state.user=loggedin_user
+            st.write("loggedin successfully")
+            st.rerun()
+       
      
         
-with signup:
+def signup_function():
     st.header("SignUp")
     with st.form("SignUp_form"):
         name=st.text_input("Name")
@@ -35,7 +48,22 @@ with signup:
             conn_obj.commit()
             st.write("user added successfully ")
             
+if st.session_state.user==None:
+    login,signup=st.tabs(
+                ["Login","SignUp"]  
+                )
+    with signup:
+        signup_function()
+    with login:
+        login_function()    
+         
+    
+else:
+    dashboard()    
+            
         
         
-        
-        
+# cursor_obj.execute("show tables")
+# dbs=cursor_obj.fetchall()
+# for db in dbs:
+#     st.write(db)
